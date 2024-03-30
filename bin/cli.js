f1405a99f187e3c55ef0d99b6c031605733a283e
Vigneshwar Pasupathi <vigneshdemitro@gmail.com>
2024-03-30 16:11:06 +0530

    update: cli.js to install deps into client

--- bin/cli.js
+++ bin/cli.js
@@ -1,34 +1,101 @@
-import { execSync } from 'node:child_process';
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { readFile, writeFile } = require('fs/promises');
+
+const packageName = 'node-express-ts-boilerplate';
+const gitRepo = `https://github.com/vigneshdemitro/${packageName}.git`;
+
+const gitCommand = `git clone --depth 1 ${gitRepo} ${repoName}`;
+const installDeps = `cd ${repoName} && npm install`;
+
+const checkInputs = () => {
+    if (process.argv.length < 3) {
+        console.log('Kindly provide app name');
+        console.log(`Example npx ${packageName} my-app`);
+        process.exit(1);
+    }
+}
+
+const parseInputs = () => {
+    const projectName = process.argv[2];
+    const currentPath = process.cwd();
+    const projectPath = path.join(currentPath, projectName);
+
+    return { projectName, projectPath };
+}
+
+const createProjectFolder = ({ projectPath, projectName }) => {
+    try {
+        fs.mkdirSync(projectPath);
+    } catch (error) {
+        if (error.code === 'EEXIST') {
+            console.log(
+                `The folder ${projectName} already exist in the current directory, please give it another name.`,
+            );
+        } else {
+            console.log(error);
+        }
+        process.exit(1);
+    }
+};
+
 
 const runCommand = (command) => {
     try {
         execSync(command, { stdio: 'inherit', encoding: 'utf-8' });
+        return true;
     } catch (error) {
-        console.error(`Error occured while executing : ${command}`, error)
+        console.error(`Error occured while executing : ${command}`, error);
+        return false;
     }
 }
 
-const defaultRepoName = 'server';
-const repoName = process.argv[2] || defaultRepoName;
+const cloneRepo = (projectPath) => {
+    console.log('Downloading files...');
+    runCommand(gitCommand);
+    process.chdir(projectPath);
+};
 
-const gitCommand = `git clone --depth 1 https://github.com/vigneshdemitro/node-express-ts-boilerplate.git ${repoName}`;
-const installDeps = `cd ${repoName} && npm install`;
-
-console.log(`Cloning into ${repoName}`);
-const checkedOut = runCommand(gitCommand);
-if (!checkedOut) {
-    console.error(`Error in cloning repo`);
-    process.exit(-1);
+const installDependencies = () => {
+    console.log('Installing Dependencies');
+    runCommand(`npm install`);
 }
 
-console.log(`Installing dependencies in ${repoName}`);
-const installPackages = runCommand(installDeps);
+const removeExtraFiles = async ({ projectName, projectPath }) => {
+    console.log('Tidying up the directory');
+    fs.rmSync(path.join(projectPath, '.git'), { recursive: true });
+    fs.rmSync(path.join(projectPath, 'bin'), { recursive: true });
+    let packageJSONData = await readFile(path.join(projectPath, 'package.json'));
+    const packageJSON = JSON.parse(packageJSONData);
+    packageJSON.name = projectName;
+    packageJSON.version = '1.0.0';
+    packageJSON.description = `Created with @vigneshdemitro/${packageName}`;
+    delete packageJSON['keywords'];
+    delete packageJSON['bugs'];
+    delete packageJSON['homepage'];
+    delete packageJSON['repository'];
+    delete packageJSON['bin'];
+    packageJSONData = JSON.stringify(packageJSON, null, 2);
+    await writeFile(path.join(projectPath, 'package.json'), packageJSONData);
+};
 
-if (!installPackages) {
-    console.error(`Error in installing dependencies`);
-    process.exit(-1);
+async function main() {
+    try {
+        checkInputs();
+        const { projectName, projectPath } = parseInputs();
+        createProjectFolder({ projectName, projectPath });
+        cloneRepo(projectPath);
+        await removeExtraFiles({ projectName, projectPath });
+        installDependencies()
+        console.log(`Congratulations, your app is ready to use`)
+        console.log(`Start app: npm run dev`)
+    } catch (error) {
+        console.error(`Unexpected error happened`, error);
+    }
+
 }
 
-console.log(`Congragulations your app is ready to use`)
-console.log(`Navigate: cd ${repoName}`)
-console.log(`Start app: npm run dev`)
+main();
